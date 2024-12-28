2fd78227a4807c0fc900639a5e9a3598128c7ca5
Avi <tveeramoothoo@gmail.com>
2024-12-28 06:02:29 -0500

    Added oqkds repo

---
+++ Dockerfile
@@ -0,0 +1,42 @@
+# Build from MATLAB base image
+FROM mathworks/matlab:latest
+
+# Set variables
+ENV MLM_WEB_LICENSE=True
+ENV ACCEPT_EULA=Y
+
+# Install required tools
+USER root
+RUN apt-get update && apt-get install -y \
+    wget \
+    unzip \
+    && rm -rf /var/lib/apt/lists/*
+
+# Create the Dependency folder
+WORKDIR /usr/src/matlab-code/Dependency
+
+# Download and unzip required files into the Dependency folder
+RUN wget -O GNQKDcodesALLfiles.zip https://www.math.uwaterloo.ca/~hwolkowi/henry/reports/ZGNQKDmainsolverUSEDforPUBLCNJuly31/GNQKDcodesALLfiles.zip \
+    && unzip GNQKDcodesALLfiles.zip -d GNQKDcodesALLfiles \
+    && rm GNQKDcodesALLfiles.zip
+
+RUN wget -O cvx.zip https://github.com/cvxr/CVX/releases/download/2.2.2/cvx.zip \
+    && unzip cvx.zip -d cvx \
+    && rm cvx.zip
+
+RUN wget -O QETLAB-master.zip https://github.com/nathanieljohnston/QETLAB/archive/refs/heads/master.zip \
+    && unzip QETLAB-master.zip -d QETLAB \
+    && rm QETLAB-master.zip
+
+# Copy your script into the image
+COPY . /usr/src/matlab-code
+
+# Grant permission to the matlab user
+RUN chown -R 1000:1000 /usr/src/matlab-code
+
+# Set the working directory and user
+WORKDIR /usr/src/matlab-code
+USER matlab
+
+# Run the main MATLAB script when the container starts
+CMD ["matlab", "-batch", "dummy"]
---
+++ MathSolverModules/OQCTFrankWolfe2StepSolver/HelperFunctions/primalDfep.m
@@ -0,0 +1,44 @@
+function Dfval = primalDfep(perturbation, rho, keyProj, krausOperators,safeCutOff)
+% primalDfep Computes the value [nabla f_epsilon(rho)], where the epsilon
+% value is carefully chosen perturbation. To ensure the gradient exists
+% (use perturbationChannelEpsilon). The gradient follows the numerator
+% convention for matrix derivatives.
+%
+% Inputs:
+% * perturbation: The amount to perturb by.
+% * rho: The density matrix at which to calculate the value.
+% * keyMap: The key map currently being used in the solver.
+% * krausOperators: Kraus operators for the G map currently being used.
+% * safeCutOff: Cut-off for the logmsafe function
+%
+% Outputs: 
+% * Dfval: The value of the gradient at the given value of rho. This uses
+%   numerator convention.
+%
+% See also primalDf, primalfep, FW2StepSolver
+arguments
+    %minimial checks just to make sure cells are formatted in the correct
+    %orientation.
+    perturbation (1,1) double {mustBeInRange(perturbation,0,1)}
+    rho (:,:) double {mustBeHermitian}
+    keyProj (:,1) cell %checks are too complex for this.
+    krausOperators (:,1) cell %checks are too complex for this.
+    safeCutOff (1,1) double {mustBePositive} = 1e-14;
+end
+
+gRho = ApplyMap(rho,krausOperators);
+zRho = ApplyMap(gRho, keyProj);
+
+gRho = perturbationChannel(gRho, perturbation);
+zRho = perturbationChannel(zRho, perturbation);
+
+% log and perturb again. (The second perturbation is required)
+logGRho = perturbationChannel(logmsafe(gRho,safeCutOff), perturbation);
+logZRho = perturbationChannel(logmsafe(zRho,safeCutOff), perturbation);
+
+%Apply G^\dagger
+Dfval = ApplyMap(logGRho-logZRho,DualMap(krausOperators));
+
+% ensure Dfval is hermitian
+Dfval = (Dfval+Dfval')/2;
+end
